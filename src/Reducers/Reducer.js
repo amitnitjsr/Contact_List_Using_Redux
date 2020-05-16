57e859edd30f50aea4446a076242c6d6df85f2c2
amitnitjsr <amitnips007@gmail.com>
2020-05-16 11:37:59 +0530

    Reducer in changes

--- src/Reducers/Reducer.js
+++ src/Reducers/Reducer.js
@@ -7,10 +7,13 @@ const iState = {
 const reducer = (state = iState, action) => {
     switch (action.type) {
         case "pushList":
-            console.log('pushList')
-            const list = [...state.list, { id: (state.list.length === 0) ? 1 : state.list[state.list.length - 1].id + 1, name: action.payload.name, phone: parseInt(action.payload.phone) }];
+            const list = [...state.list, {
+                id: (state.list.length === 0) ? 1 : state.list[state.list.length - 1].id + 1,
+                name: action.payload.name, phone: (action.payload.phone),
+                address: action.payload.address, company: action.payload.company, email: action.payload.email
+            }];
             state.list = list;
-            console.log('pushList', list, state)
+            console.log('state', state)
             return state;
 
         case "deleteListById":
@@ -25,10 +28,6 @@ const reducer = (state = iState, action) => {
                 // ages = state.list.filter(data => data.id !== val[0])
                 // console.log('filter', state.list.filter(data => data.id !== val[0]))
             })
-        // var newArray = array.filter(function (item) {
-        //     return condition;
-        // });
-
         default:
             return state;
     }
